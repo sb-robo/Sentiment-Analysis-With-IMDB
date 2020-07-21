450e30db2863a54d3a6ba0366613b79e73520239
Suraj Biswas <reetbiswas503@gmail.com>
2020-07-21 22:33:53 +0530

    Clean Text/Text preprocessing

---
+++ Data Pre-procss.py
@@ -0,0 +1,6 @@
+from nltk.tokenize import RegexpTokenizer
+from nlppreprocess import NLP
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+import numpy as np
+import os
